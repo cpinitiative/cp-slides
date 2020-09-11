a6337d8e618a25a91ceb8e431bf20440ca36bc2a
Nathan Wang <nathan.r.wang@gmail.com>
2020-09-10 18:13:07 -0700

    content: add sliding window deck

---
+++ src/components/mdx-deck/ListSteps.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import useSteps from "gatsby-theme-mdx-deck/src/hooks/use-steps";
+
+export const ListSteps = props => {
+  const listItems = React.Children.toArray(props.children.props.children)
+  const step = useSteps(listItems.length)
+  console.log(listItems, step);
+  const newEl = React.cloneElement(props.children, {
+    children: listItems.map((child, i) =>
+      React.cloneElement(child, {
+        style: {
+          visibility: i < step ? 'visible' : 'hidden',
+        },
+      })
+    )
+  });
+
+  return newEl;
+}
+
+export default ListSteps
