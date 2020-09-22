0a69f4fc6c81c6d7af0750317b6e2d8449fdfa2f
Nathan Wang <nathan.r.wang@gmail.com>
2020-09-22 08:56:38 -0700

    feat: set max width of slides to be 80%

--- gatsby-theme-mdx-deck/src/components/slide.js
+++ gatsby-theme-mdx-deck/src/components/slide.js
@@ -31,43 +31,48 @@ export const Slide = ({ slide, index, preview, ...props }) => {
 
   return (
     <Context.Provider value={context}>
-      <div
-        {...(!preview ? swipeProps : {})}
-        sx={{
-          boxSizing: 'border-box',
-          width: '100%',
-          height: context.mode === modes.print ? '100vh' : '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          overflow: 'hidden',
-          position: 'relative',
-          color: 'text',
-          bg: 'background',
-          variant: 'styles.Slide',
-
-        }}>
-        {slide}
-      </div>
-      <a
-        href="https://joincpi.org/"
-        target="_blank"
-        sx={{
-          position: 'fixed',
-          top: '2rem',
-          right: '2rem',
-          color: 'white',
-        }}
-      >
-        <Img
-          fixed={data.img.childImageSharp.fixed}
-          alt="CP Initiative"
+      <div sx={{
+        bg: 'background',
+        height: context.mode === modes.print ? '100vh' : '100%',
+      }}>
+        <div
+          {...(!preview ? swipeProps : {})}
           sx={{
-            maxHeight: "12rem"
+            boxSizing: 'border-box',
+            width: '80%',
+            margin: "0 auto",
+            height: context.mode === modes.print ? '100vh' : '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            overflow: 'hidden',
+            position: 'relative',
+            color: 'text',
+            bg: 'background',
+            variant: 'styles.Slide',
+          }}>
+          {slide}
+        </div>
+        <a
+          href="https://joincpi.org/"
+          target="_blank"
+          sx={{
+            position: 'fixed',
+            top: '2rem',
+            right: '2rem',
+            color: 'white',
           }}
-        />
-      </a>
+        >
+          <Img
+            fixed={data.img.childImageSharp.fixed}
+            alt="CP Initiative"
+            sx={{
+              maxHeight: "12rem"
+            }}
+          />
+        </a>
+      </div>
     </Context.Provider>
   )
 }
